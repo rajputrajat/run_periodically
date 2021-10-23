e7bfd0eeb6312f98d99e7ac4c20ad0e95ab1d979
Rajat Rajput <rajputrajat@gmail.com>
2021-10-23 16:01:39 +0530

    deciding trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
+//! Create a service which will call your runnable periodically after specified time
+#![warn(missing_docs)]
+
 use log::trace;
+
+/// for passing milli-seconds
+pub struct MilliSeconds(usize);
+
+/// Service runner functionality
+pub trait ServiceRunner {
+    /// attach your runned and mentioned call back period
+    fn attach_runner<F>(run_again_in: MilliSeconds, runner: F)
+    where
+        F: Fn();
+}
